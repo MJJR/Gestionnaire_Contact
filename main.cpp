032b07d0be2557a84b88b9329c65be4c7eb6c10c
Etienne <etienne.dm@outlook.fr>
2022-02-10 16:29:06 +0100

    Update gitignore - Suppression test console

--- main.cpp
+++ main.cpp
@@ -8,17 +8,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    Adresse* a1 = new Adresse("126 rue jean mermoz","Toulouse", 31000);
-    Contacts * p1 = new Prives("Jean","Michel",'M',a1,'C',"12/11/1985");
-    Contacts * p2 = new Professionnels("Jean","Michel",'M',a1,"Abylsen","abylsen@gmail.com");
-
-    p1->affiche();
-    p2->affiche();
-
-    delete a1;
-    delete p1;
-    delete p2;
-
     w.show();
 
 
