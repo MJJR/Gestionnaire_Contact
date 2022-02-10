fbf4ba8c8aa5963e341c2b2335bf6f701ff9b305
MJJR <matthieu.redor@hotmail.fr>
2022-02-10 12:00:33 +0100

    Ajout de la classe contacts avec getter setter (cette fois c'est bon)

--- contacts.cpp
+++ contacts.cpp
@@ -4,3 +4,60 @@ Contacts::Contacts()
 {
 
 }
+
+QString Contacts::getAdresse() const
+{
+    return adresse;
+}
+
+void Contacts::setAdresse(const QString &value)
+{
+    adresse = value;
+}
+
+bool Contacts::getSexe() const
+{
+    return sexe;
+}
+
+void Contacts::setSexe(bool value)
+{
+    sexe = value;
+}
+
+QString Contacts::getPrenom() const
+{
+    return prenom;
+}
+
+void Contacts::setPrenom(const QString &value)
+{
+    prenom = value;
+}
+
+QString Contacts::getNom() const
+{
+    return nom;
+}
+
+void Contacts::setNom(const QString &value)
+{
+    nom = value;
+}
+
+int Contacts::getId() const
+{
+    return id;
+}
+
+void Contacts::setId(int value)
+{
+    id = value;
+}
+
+void Contacts::affiche()
+{
+
+}
+
+
--- contacts.h
+++ contacts.h
@@ -1,11 +1,33 @@
 #ifndef CONTACTS_H
 #define CONTACTS_H
 
+#include <QString>
+
+
 
 class Contacts
 {
-public:
+protected:
     Contacts();
+    int id;
+    QString nom;
+    QString prenom;
+    bool sexe;
+    QString adresse;
+
+public:
+    int getId() const;
+    void setId(int value);
+    QString getNom() const;
+    void setNom(const QString &value);
+    QString getPrenom() const;
+    void setPrenom(const QString &value);
+    bool getSexe() const;
+    void setSexe(bool value);
+    QString getAdresse() const;
+    void setAdresse(const QString &value);
+
+    virtual void affiche();
 };
 
 #endif // CONTACTS_H
