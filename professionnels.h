df85d0a330aa3df617725255ddf74a14cd9d86b4
Dimitri <Dimitri.p53@live.fr>
2022-02-10 11:52:46 +0100

    Squellette Professionnels Fix

--- professionnels.h
+++ professionnels.h
@@ -1,11 +1,26 @@
 #ifndef PROFESSIONNELS_H
 #define PROFESSIONNELS_H
 
+#include <QString>
+#include "Contacts.h"
 
 class Professionnels : public Contacts
 {
+
+private:
+    QString nomEntreprise;
+    QString email;
+
 public:
     Professionnels();
+
+    void affiche() override;
+
+    QString getEmail() const;
+    void setEmail(const QString &value);
+
+    QString getNomEntreprise() const;
+    void setNomEntreprise(const QString &value);
 };
 
 #endif // PROFESSIONNELS_H
